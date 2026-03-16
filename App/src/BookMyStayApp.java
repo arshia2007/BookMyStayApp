8b6118909b98a58dbb56c6169d207069cce29f57
Arshia <guptaarshia1@gmail.com>
2026-03-16 09:12:29 +0530

    updated uc3

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,10 +1,11 @@
-class BookMyStayApp{
+class BookMyStayApp {
 
     public static void main(String[] args){
+
         System.out.println("Welcome to hostel booking management system");
         System.out.println("System initialized successfully.");
 
-//        UC2 - Basic Room Types & Static Availability
+        // UC2 - Basic Room Types & Static Availability
         DoubleRoom doubleRoom = new DoubleRoom();
         SuiteRoom suiteRoom = new SuiteRoom();
 
@@ -13,5 +14,25 @@ class BookMyStayApp{
 
         System.out.println("Suite Room Details:");
         suiteRoom.displayRoomDetails();
+
+
+        // UC3 - Centralized Room Inventory Management
+        RoomInventory inventory = new RoomInventory();
+
+        Room doubleRoomInv = new DoubleRoom();
+        Room suiteRoomInv = new SuiteRoom();
+
+        System.out.println("Hotel Room Inventory Status\n");
+
+        System.out.println("Double Room:");
+        doubleRoomInv.displayRoomDetails();
+        System.out.println("Available Rooms: " +
+                inventory.getRoomAvailability().get("Double Room"));
+        System.out.println();
+
+        System.out.println("Suite Room:");
+        suiteRoomInv.displayRoomDetails();
+        System.out.println("Available Rooms: " +
+                inventory.getRoomAvailability().get("Suite Room"));
     }
 }
